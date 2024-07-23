7ac1e31399267f51b6471fe4985e49f73d79cad5
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-07-23 16:26:58 +0100

    React Components - Exercise 1

---
+++ 04_React Fundamentals/Scratchpad/react-template/src/App.jsx
@@ -0,0 +1,10 @@
+import Profile from "./components/Profile";
+import "./App.css";
+
+function App() {
+    return (
+        <Profile />
+    );
+}
+
+export default App;
---
+++ 04_React Fundamentals/Scratchpad/react-template/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+    return (
+        <div id="profile">
+            <h1 id="name">Quackie Makers</h1>
+
+            <p id="job">Makers' favourite rubber duck</p>
+            <p id="birthdate">2013</p>
+        </div>
+    );
+};
+
+export default Profile;
