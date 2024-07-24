4577e4a9d1d73185d58f14c33f95c42a3b8c9e0b
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-07-24 01:18:28 +0100

    Passing Data to Components (Props) - Exercise 2

---
+++ 04_React Fundamentals/Scratchpad/react-template/src/components/Product.jsx
@@ -0,0 +1,12 @@
+const Product = (props) => {
+    return (
+        <div id="product">
+            <h1 id="name">{props.name}</h1>
+
+            <p id="description">{props.description}</p>
+            <p id="price">{props.price}</p>
+        </div>
+    );
+};
+
+export default Product;
