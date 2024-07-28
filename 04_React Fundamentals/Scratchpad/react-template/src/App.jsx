da3471a9d099e0fb2f6dc607d82ed1bad1b59b53
Aivaras Voleika <154103843+aivocode@users.noreply.github.com>
2024-07-28 12:38:24 +0100

    Keeping State in React - Exercise 1

--- 04_React Fundamentals/Scratchpad/react-template/src/App.jsx
+++ 04_React Fundamentals/Scratchpad/react-template/src/App.jsx
@@ -1,18 +1,22 @@
+
 import Profile from "./components/Profile";
 import Product from "./components/Product";
 import Recipe from "./components/Recipe";
+import Counter from "./components/Counter";
 
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Counter />
+      <Counter />
       <Profile name="Doge" job="Nuclear Physicist" birthdate="2000" />
 
       <Product name="Electric Fan" description="It spins" price={25} />
       <Product name="Vacuum" description="It vacuums" price={100} />
 
-      <Recipe title="Finnish cinnamon buns" type="dessert" duration={60}/>
+      <Recipe title="Finnish cinnamon buns" type="dessert" duration={60} />
     </>
   );
 }
